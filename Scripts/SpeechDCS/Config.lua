837516441252842257b2c438b642b92c891cd714
Crivi <tanamg@gmail.com>
2018-11-15 17:56:08 +0000

    .

--- Scripts/SpeechDCS/Config.lua
+++ Scripts/SpeechDCS/Config.lua
@@ -1,16 +1,3 @@
---dofile(lfs.writedir()..[[Scripts\SpeechDCS\Lib\Util.lua]])
---dofile(lfs.writedir()..[[Scripts\SpeechDCS\Lib\Math.lua]])
---dofile(lfs.writedir()..[[Scripts\SpeechDCS\Lib\List.lua]])
---dofile(lfs.writedir()..[[Scripts\SpeechDCS\Lib\Stack.lua]])
---dofile(lfs.writedir()..[[Scripts\SpeechDCS\Lib\Queue.lua]])
---dofile(lfs.writedir()..[[Scripts\SpeechDCS\Lib\Command.lua]])
---dofile(lfs.writedir()..[[Scripts\SpeechDCS\Lib\Packet.lua]])
---dofile(lfs.writedir()..[[Scripts\SpeechDCS\Lib\PacketQueue.lua]])
---dofile(lfs.writedir()..[[Scripts\SpeechDCS\Lib\UdpNetwork.lua]])
---dofile(lfs.writedir()..[[Scripts\SpeechDCS\Lib\Controller.lua]])
-
---dofile(lfs.writedir()..[[Scripts\SpeechDCS\Lib\Mod\Gazelle.lua]])
-
 dofile(lfs.writedir()..[[Scripts\SpeechDCS\SpeechDCS.lib]])
 
 Config = {}
